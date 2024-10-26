be6338cd53208927505da61fd5f0dc656035136a
rajeshponnala <rajeshreddyponnala@gmail.com>
2024-10-27 01:44:26 +0530

    Add Dbcontext

---
+++ Bookify.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Bookify.Domain.Abstractions;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookify.Infrastructure
+{
+    public sealed class ApplicationDbContext : DbContext, IUnitOfWork
+    {
+        public ApplicationDbContext(DbContextOptions options): base(options)
+        {
+        }
+    }
+}
