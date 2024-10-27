05820bc229bad39fcb485bf2ac1357f4508dcbfa
rajeshponnala <rajeshreddyponnala@gmail.com>
2024-10-27 12:18:39 +0530

    Implement Publishing domain events in ApplicationDb Context

--- Bookify.Infrastructure/ApplicationDbContext.cs
+++ Bookify.Infrastructure/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Bookify.Domain.Abstractions;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -10,13 +11,38 @@ namespace Bookify.Infrastructure
 {
     public sealed class ApplicationDbContext : DbContext, IUnitOfWork
     {
-        public ApplicationDbContext(DbContextOptions options): base(options)
+        private readonly IPublisher _publisher;
+        public ApplicationDbContext(DbContextOptions options, IPublisher publisher): base(options)
         {
+            _publisher = publisher;
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
             base.OnModelCreating(modelBuilder);
+        }
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            var result = await base.SaveChangesAsync(cancellationToken);
+            await PublishDomainEventsAsync();
+            return result;
+        }
+
+        private async Task PublishDomainEventsAsync()
+        {
+            var domainEvents = ChangeTracker
+                .Entries<Entity>()
+                .Select(entry => entry.Entity)
+                .SelectMany(entity => {
+                    var domainEvents = entity.GetDomainEvents();
+                    entity.ClearDomainEvents();
+                    return domainEvents;
+                 })
+                .ToList();
+            foreach(var domainEvent in domainEvents)
+            {
+                await _publisher.Publish(domainEvent);
+            }
         }
     }
 }
