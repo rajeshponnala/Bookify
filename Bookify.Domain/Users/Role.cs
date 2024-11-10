31a4c431dde5200533ac88752e1b9b4430fd1acc
rajeshponnala <rajeshreddyponnala@gmail.com>
2024-11-11 02:15:43 +0530

    Update User
    - Add Role Model
    
    - Add Role in User Model

---
+++ Bookify.Domain/Users/Role.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookify.Domain.Users
+{
+    public sealed class Role
+    {
+        public static readonly Role Registered = new(1, "Registered");
+        public Role(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public int Id { get; init; }
+        public string Name { get; init; } = String.Empty;
+
+        public ICollection<User> Users { get; init; } = new List<User>();
+    }
+}
--- Bookify.Domain/Users/User.cs
+++ Bookify.Domain/Users/User.cs
@@ -10,6 +10,7 @@ namespace Bookify.Domain.Users
 {
     public sealed class User : Entity
     {
+        private readonly List<Role> _roles = new();
         private User(Guid id, FirstName firstName, LastName lastName, Email email): base(id)
         {
             FirstName = firstName;
@@ -29,10 +30,13 @@ namespace Bookify.Domain.Users
         public Email Email { get; private set; }
         public string IdentityId { get; private set; } = String.Empty;
 
+        public IReadOnlyCollection<Role> Roles => _roles.ToList();
+
         public static User Create(FirstName firstName, LastName lastName, Email email)
         {
             var user = new User(Guid.NewGuid(), firstName, lastName, email);
             user.RaiseDomainEvent(new UserCreatedDomainEvent(user.Id));
+            user._roles.Add(Role.Registered);
             return user;
         }
 
