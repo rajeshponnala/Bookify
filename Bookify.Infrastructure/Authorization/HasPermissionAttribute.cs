fa0b2f44b729db575b42b09ba8924ad64976dc16
rajeshponnala <rajeshreddyponnala@gmail.com>
2024-11-15 00:06:53 +0530

    Create a cusyom permission attribute

---
+++ Bookify.Infrastructure/Authorization/HasPermissionAttribute.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookify.Infrastructure.Authorization
+{
+    public class HasPermissionAttribute : AuthorizeAttribute
+    {
+        public HasPermissionAttribute(string permission) : base(permission)
+        {
+        }
+    }
+}
